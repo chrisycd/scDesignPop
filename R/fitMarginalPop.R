91202c86134d24b786e1eeb388d48b7a6a997423
chrisycd <furies2013@gmail.com>
2024-09-16 23:17:14 +0000

    Revert cell barcode names; added option.

--- R/fitMarginalPop.R
+++ R/fitMarginalPop.R
@@ -9,7 +9,7 @@
 #' @param model_family a string scalar to specify model fitting used.
 #' @param interact_colnames a string scalar or vector for the variable names that
 #'     have first-order interaction with SNP genotypes.
-#' @param parallelization a string scalar specifying the type of parallization
+#' @param parallelization a string scalar specifying the type of parallelization
 #'     used during marginal fitting.
 #' @param n_threads positive integer value (greater or equal to 1) to specify the
 #'     number of CPU threads used in parallelization.
@@ -29,6 +29,8 @@
 #'     \code{force_formula = FALSE} and \code{length(geno_interact_names) > 0}.
 #' @param data_maxsize a positive numeric value used to set max data_list size
 #'     in GiB unit. Used only when \code{parallelization = 'future'}.
+#' @param keep_cellnames a logical scalar for whether to keep cell barcode names.
+#'     If \code{keep_cellnames = TRUE}, the memory will be larger. The default is FALSE.
 #'
 #' @return a list of named features, each containing a list with the following items:
 #' \describe{
@@ -36,6 +38,8 @@
 #'      \item{\code{time}}{a numeric scalar for the elapsed time to fit the given feature.}
 #'      \item{\code{snp_cov}}{a string scalar or vector of SNP ids used in the
 #'          fit for given feature.}
+#'      \item{\code{model_attr}}{a list of attributes extracted from each model. NOT
+#'          currently implemented.}
 #'      \item{\code{removed_cell}}{a string scalar or vector of the cell names
 #'          removed due to low-variance (NOT currently implemented).}
 #' }
@@ -56,8 +60,8 @@ fitMarginalPop <- function(data_list,
                            filter_snps = TRUE,
                            snpvar_thres = 0,
                            force_formula = FALSE,
-                           data_maxsize = 1
-                           ) {
+                           data_maxsize = 1,
+                           keep_cellnames = FALSE) {
 
     # check cell covariates
     assertthat::assert_that(assertthat::has_name(data_list[["covariate"]],
@@ -74,6 +78,7 @@ fitMarginalPop <- function(data_list,
     important_features <- data_list[["important_features"]]
     eqtl_features <- names(data_list[["eqtl_geno_list"]])
     sce_features <- colnames(data_list[["count_mat"]])
+    cell_names <- rownames(data_list[["count_mat"]])
 
     stopifnot("Features in count_mat and eqtl_geno_list do not match. Please check input!" =
                   checkVectorEqual(sce_features, eqtl_features, ignore_order = TRUE))
@@ -90,6 +95,13 @@ fitMarginalPop <- function(data_list,
 
     stopifnot("loc_colname, snp_colname, or cellstate_colname is missing in eqtl_geno_list. Please check input!" =
                   checkVectorContain(c(loc_colname, snp_colname, cellstate_colname), eqtl_colnames))
+
+
+    # remove cell barcodes
+    if(!keep_cellnames) {
+        rownames(data_list[["count_mat"]]) <- NULL
+        rownames(data_list[["covariate"]]) <- NULL
+    }
 
 
     ## run marginal fitting
@@ -144,6 +156,9 @@ fitMarginalPop <- function(data_list,
 
     # store gene ids as list names
     names(marginal_list) <- sce_features
+
+    #
+    # marginal_list[["cell_names"]] <- cell_names
 
     # clean up
     rm(data_list)
@@ -255,6 +270,9 @@ fitModel <- function(feature_name,
                     # Note: use ziformula = ~1 for zero-inflation
 
                     # model[["frame"]] <- c()  # remove training dataframe
+                    model_attr <- list("class" = attr(model[["frame"]], "class"),
+                                       "terms" = attr(model[["frame"]], "terms"),
+                                       "names" = attr(model[["frame"]], "names"))
                     # model[["frame"]] <- model[["frame"]] %>%    # retain response vec and snp covariate df
                     #     dplyr::select(c("response", dplyr::all_of(snp_cov)))
 
@@ -290,6 +308,7 @@ fitModel <- function(feature_name,
     return(list("fit" = glmmTMB.fit,
                 "time" = elap_time,
                 "snp_cov" = snp_cov,  # snp covariates used in model fitting
+                "model_attr" = model_attr,
                 "removed_cell" = removed_cell)
            )
 }
@@ -330,8 +349,6 @@ constructDesignMatrix <- function(response_vec,
                                   filter_snps = TRUE,
                                   snpvar_thres = 0,
                                   cleanup = TRUE) {
-
-    cell_names <- rownames(cellcov_df)
 
     # construct a sample by snp df using indiv label
     firstsnp_indx <- which(colnames(eqtlgeno_df) == loc_colname) + 1L  # last column preceding genotypes
@@ -374,18 +391,17 @@ constructDesignMatrix <- function(response_vec,
     # construct design matrix df for marginal fitting
     # dmat_df <- data.frame("response" = response_vec) %>%
     #     dplyr::bind_cols(., cellcov_df) %>%
-    dmat_df <- cellcov_df %>%
-        dplyr::left_join(., geno_df, by = indiv_colname) %>%  # TODO: keep cell barcodes as rownames
+    dmat_df <- cellcov_df %>%  # keeps row names
+        dplyr::left_join(., geno_df, by = indiv_colname) %>%
         dplyr::mutate(dplyr::across(tidyselect::where(is.character), as.factor))
         # dplyr::mutate(!!rlang::sym(indiv_colname) := as.factor(!!rlang::sym(indiv_colname)))  # old code
 
-    # add response variabl to design matrix
+    # add response variable to design matrix
     if(!is.null(response_vec)) {
         dmat_df <- dmat_df %>%
             dplyr::bind_cols(data.frame("response" = response_vec), .)
     }
 
-    rownames(dmat_df) <- cell_names
 
     # clean up
     if(cleanup) {
